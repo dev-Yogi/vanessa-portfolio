4a326e16fc7e15152c67665b964014e80ba3f76c
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2026-04-02 15:23:43 -0500

    add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Syne } from 'next/font/google'
 import { DM_Sans } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
